2c564b1754d1a491cc45eba8d6763f7231413a8c
hyf_syp <hyf@syp.com>
2019-08-06 17:41:23 +0800

    简单的粒子动画

---
+++ app/src/main/java/com/example/testgrav/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.testgrav;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author admin
+ * desc: 简单的粒子动画
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
